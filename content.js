29dd22cad4f3cfad20e16490d23ea7bac71212a2
Casa <44913286+casadevelop@users.noreply.github.com>
2023-02-06 01:29:12 +0900

    インプット要素が取得できない場合のエラーを修正

--- content.js
+++ content.js
@@ -1,19 +1,21 @@
 var elementsIndex = -1;
 
 document.addEventListener('keydown', (event) => {
-  if(event.code == "KeyQ" && event.ctrlKey){
+  if (event.code == "KeyQ" && event.ctrlKey) {
     var inputElements = document.querySelectorAll("input:not([type='hidden'])");
-    if(event.shiftKey){
-      elementsIndex <= 0
-        ? elementsIndex = inputElements.length - 1
-        : elementsIndex--
-    }else{
-      elementsIndex == inputElements.length - 1
-        ? elementsIndex = 0
-        : elementsIndex++;
-    }
-    inputElements[elementsIndex].select();
-    console.log(elementsIndex);
-    console.log(inputElements[elementsIndex]);    
+      if (event.shiftKey) {
+        elementsIndex <= 0
+          ? elementsIndex = inputElements.length - 1
+          : elementsIndex--
+      } else {
+        elementsIndex == inputElements.length - 1
+          ? elementsIndex = 0
+          : elementsIndex++;
+      }
+      if (inputElements[elementsIndex] !== undefined){
+        inputElements[elementsIndex].select();
+      }
+      console.log(elementsIndex);
+      console.log(inputElements[elementsIndex]);
   }
 });
